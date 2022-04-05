f43a1a4ff1f5fd21627d9a537d197c7dd22a2fe4
Babar Khan <babar.khan@couchbase.com>
2022-04-05 16:47:41 +0100

    testing release output folder part 2

---
+++ src/extension.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "couchbase-sdk-snippets" is now active!');
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
